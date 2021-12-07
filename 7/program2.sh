89bd7a472b66a8dba018f71832fa1f95a0088684
Jan Tlusty <jtlusty@purestorage.com>
2021-12-07 12:01:17 +0100

    unsued variable
    Signed-off-by: Jan Tlusty <jtlusty@purestorage.com>

--- 7/program2.sh
+++ 7/program2.sh
@@ -9,9 +9,7 @@ for i in $(seq 0 $max_position); do
   cost=0
   for p in $positions; do
     delta=$(echo $(($i-$p)) | tr -d '-')
-    if [[ $delta -ne 0 ]]; then
-      cost="$cost+$(echo $(seq -s '+' 1 $delta) | bc)"
-    fi
+      [[ $delta -ne 0 ]] && cost="$cost+$(echo $(seq -s '+' 1 $delta) | bc)"
   done
   _cost=$(echo "$cost" | bc)
   [[ $_cost -lt $min_cost ]] && min_cost=$_cost
