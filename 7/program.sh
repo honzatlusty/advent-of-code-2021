b115b6ccd97aeefd950e6000c3b3f7c15679d2e4
Jan Tlusty <jtlusty@purestorage.com>
2021-12-07 12:18:16 +0100

    unsued variable
    Signed-off-by: Jan Tlusty <jtlusty@purestorage.com>

--- 7/program.sh
+++ 7/program.sh
@@ -7,9 +7,8 @@ positions=$(tr ',' ' ' < $input)
 max_position=$(echo $positions | tr ' ' '\n' | sort -n | tail -n1)
 
 for i in $(seq 0 $max_position); do
-  cost=$(tr ',' '\n' < $input  | while read a; do echo  "$(($a-$i))" | tr -d '-'; done | tr '\n' '+' | sed 's?+$??')
-  _cost=$(echo "$cost" | bc)
-  [[ $_cost -lt $min_cost ]] && min_cost=$_cost
+  cost=$(($(tr ',' '\n' < $input  | while read a; do echo  "$(($a-$i))" | tr -d '-'; done | tr '\n' '+' | sed 's?+$??')))
+  [[ $cost -lt $min_cost ]] && min_cost=$cost
 done
 
 echo $min_cost
